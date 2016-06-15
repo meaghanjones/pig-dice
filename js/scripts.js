a0c588aa08bec6cd899fa5511cc3d0252fc7fa30
Kevin VanEvery and Meaghan Jones <kevin@>
2016-06-15 09:34:37 -0700

    add functionality to allow players to hold a roll and add to a total score

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,31 @@
 
 $(document).ready(function(){
   var scoreForTurnPlayer1 = 0;
+  var totalScorePlayer1 = 0;
   $("button#roll-player1").click(function () {
     var rollResult = roll();
     $("p#roll-result-player1").text(rollResult);
     scoreForTurnPlayer1 = addRolls(scoreForTurnPlayer1, rollResult);
     $("p#turn-score-player1").text(scoreForTurnPlayer1);
   })
+  $("button#hold-player1").click(function () {
+    totalScorePlayer1 = hold(scoreForTurnPlayer1, totalScorePlayer1);
+    $("p#total-score-player1").text(totalScorePlayer1);
+    scoreForTurnPlayer1 = 0;
+  })
+
   var scoreForTurnPlayer2 = 0;
+  var totalScorePlayer2 = 0;
   $("button#roll-player2").click(function () {
     var rollResult = roll();
     $("p#roll-result-player2").text(rollResult);
     scoreForTurnPlayer2 = addRolls(scoreForTurnPlayer2, rollResult);
     $("p#turn-score-player2").text(scoreForTurnPlayer2);
+  })
+  $("button#hold-player2").click(function () {
+    totalScorePlayer2 = hold(scoreForTurnPlayer2, totalScorePlayer2);
+    $("p#total-score-player2").text(totalScorePlayer2);
+    scoreForTurnPlayer2 = 0;
   })
 });
 
@@ -30,3 +43,8 @@ var addRolls = function (currentScore, rollResult) {
   var scoreForTurn = currentScore + rollResult;
   return scoreForTurn;
 }
+
+ var hold = function (scoreForTurn, totalScore) {
+   totalScore = totalScore + scoreForTurn;
+   return totalScore;
+ }
