1a8571181609b943e049ec8e1fd151e32d778135
Kevin VanEvery and Meaghan Jones <meaghan@>
2016-06-15 10:18:37 -0700

    add functionality to support win conditions and turns for two players

--- js/scripts.js
+++ js/scripts.js
@@ -8,35 +8,63 @@ $(document).ready(function(){
   $("button#roll-player1").click(function () {
     var rollResult = roll();
     $("p#roll-result-player1").text(rollResult);
-    scoreForTurnPlayer1 = addRolls(scoreForTurnPlayer1, rollResult);
-    $("p#turn-score-player1").text(scoreForTurnPlayer1);
-  })
+    if (rollResult === 1) {
+      scoreForTurnPlayer1 = 0;
+      $("p#turn-score-player1").text(scoreForTurnPlayer1);
+      nextTurn();
+    } else {
+      scoreForTurnPlayer1 = addRolls(scoreForTurnPlayer1, rollResult);
+      $("p#turn-score-player1").text(scoreForTurnPlayer1);
+    }
+  });
+
   $("button#hold-player1").click(function () {
     totalScorePlayer1 = hold(scoreForTurnPlayer1, totalScorePlayer1);
     $("p#total-score-player1").text(totalScorePlayer1);
+    if (totalScorePlayer1 >= 100) {
+      alert ("You Win!!!");
+    }
     scoreForTurnPlayer1 = 0;
-  })
+    nextTurn();
+  });
 
   var scoreForTurnPlayer2 = 0;
   var totalScorePlayer2 = 0;
   $("button#roll-player2").click(function () {
     var rollResult = roll();
     $("p#roll-result-player2").text(rollResult);
-    scoreForTurnPlayer2 = addRolls(scoreForTurnPlayer2, rollResult);
-    $("p#turn-score-player2").text(scoreForTurnPlayer2);
-  })
+    if (rollResult === 1){
+      scoreForTurnPlayer2 = 0;
+      $("p#turn-score-player2").text(scoreForTurnPlayer2);
+      nextTurn();
+    } else {
+      scoreForTurnPlayer2 = addRolls(scoreForTurnPlayer2, rollResult);
+      $("p#turn-score-player2").text(scoreForTurnPlayer2);
+
+      }
+  });
+
   $("button#hold-player2").click(function () {
     totalScorePlayer2 = hold(scoreForTurnPlayer2, totalScorePlayer2);
     $("p#total-score-player2").text(totalScorePlayer2);
+    if (totalScorePlayer2 >= 100){
+      alert ("You Win!!!");
+    }
     scoreForTurnPlayer2 = 0;
-  })
+    nextTurn();
+  });
 });
 
+var nextTurn = function () {
+  $("button.player1").toggle();
+  $("button.player2").toggle();
+}
 // Back end logic
+var turn = 1;
 
 var roll = function () {
   var integer = Math.floor(Math.random() * (6 - 1)) + 1;
-  return integer;
+    return integer;
 }
 
 var addRolls = function (currentScore, rollResult) {
